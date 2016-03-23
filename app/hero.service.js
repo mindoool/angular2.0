6d76c06719a121eb2563f71201f44f93f73e97d1
Minseok Kim <min3482@snu.ac.kr>
2016-03-23 23:46:07 +0900

    tutorial services completed

--- app/hero.service.js
+++ app/hero.service.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', './mock-heroes'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,17 +10,30 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, mock_heroes_1;
     var HeroService;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (mock_heroes_1_1) {
+                mock_heroes_1 = mock_heroes_1_1;
             }],
         execute: function() {
             HeroService = (function () {
                 function HeroService() {
                 }
+                HeroService.prototype.getHeroes = function () {
+                    return Promise.resolve(mock_heroes_1.HEROES);
+                };
+                HeroService.prototype.getHeroesSlowly = function () {
+                    return new Promise(function (resolve) {
+                        return setTimeout(function () { return resolve(mock_heroes_1.HEROES); }, 2000);
+                    } // 2 seconds
+                     // 2 seconds
+                    );
+                };
                 HeroService = __decorate([
                     core_1.Injectable(), 
                     __metadata('design:paramtypes', [])
